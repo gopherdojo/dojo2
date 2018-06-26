c9a4d29a6943803880050ee297a59500031148ab
kaznishi <kaz.nishimu@gmail.com>
2018-06-26 19:16:52 +0900

    searcherについてテスト実装

---
+++ kadai2/kadai2-2/imageconverter/searcher_test.go
@@ -0,0 +1,30 @@
+package imageconverter_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/gopherdojo/dojo2/kadai2/kadai2-2/imageconverter"
+)
+
+func TestRun(t *testing.T) {
+	var searcher imageconverter.Searcher
+	result := searcher.Run(imageconverter.FileInfo{Path: imageconverter.FilePath("../sample_dir1")})
+	fmt.Println(result)
+	expected := []imageconverter.FileInfo{
+		imageconverter.FileInfo{Path: imageconverter.FilePath("../sample_dir1/Octocat.jpeg")},
+		imageconverter.FileInfo{Path: imageconverter.FilePath("../sample_dir1/dummy_fuga.md")},
+		imageconverter.FileInfo{Path: imageconverter.FilePath("../sample_dir1/dummy_hoge.txt")},
+		imageconverter.FileInfo{Path: imageconverter.FilePath("../sample_dir1/sample_dir2/Octocat.jpg")},
+		imageconverter.FileInfo{Path: imageconverter.FilePath("../sample_dir1/sample_dir2/dummy_fuga.md")},
+		imageconverter.FileInfo{Path: imageconverter.FilePath("../sample_dir1/sample_dir2/dummy_hoge.txt")},
+		imageconverter.FileInfo{Path: imageconverter.FilePath("../sample_dir1/sample_dir2/sample_dir3/Octocat.png")},
+		imageconverter.FileInfo{Path: imageconverter.FilePath("../sample_dir1/sample_dir2/sample_dir3/dummy_fuga.md")},
+		imageconverter.FileInfo{Path: imageconverter.FilePath("../sample_dir1/sample_dir2/sample_dir3/dummy_hoge.txt")},
+	}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Run(../sample_dir1) failed.  expect:%s, actual:%s", expected, result)
+	}
+
+}
