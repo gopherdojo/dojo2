70d7d8b72f0d5f69b1667fc5f99a1f2f0d5d561e
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-06-27 02:56:39 +0900

    main処理からflagのパース,画像変換コマンド切り出し

---
+++ kadai2/tokunaga/cli.go
@@ -0,0 +1,82 @@
+package tokunaga
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// 終了コード
+const (
+	ExitCodeOK = iota
+	ExitCodeParseFlagError
+)
+
+type CLI struct {
+	OutStream, ErrStream io.Writer
+	to, from             string
+}
+
+var permmitedExts = []string{"png", "jpeg", "jpg"}
+
+const usage = "./imageConverter [-from=png|jpeg ] [-to=png|jpeg ] directory"
+
+// 引数処理を含めた具体的な処理
+func (c *CLI) Run(args []string) int {
+	// オプション引数のパース
+	flags := flag.NewFlagSet("imageConverter", flag.ContinueOnError)
+	flags.SetOutput(c.ErrStream)
+	flags.StringVar(&c.from, "from", "jpeg", usage)
+	flags.StringVar(&c.to, "to", "png", usage)
+
+	if err := flags.Parse(args[1:]); err != nil {
+		fmt.Fprintln(c.ErrStream, "Usage: "+string(usage))
+		return ExitCodeParseFlagError
+	}
+	if len(flags.Args()) != 1 {
+		fmt.Fprintln(c.ErrStream, "Usage: "+string(usage))
+		return ExitCodeParseFlagError
+	}
+	if !checkExtPermmited(c.from, permmitedExts) {
+		fmt.Fprintln(c.ErrStream, "-from is only png, jpeg, jpg")
+		return ExitCodeParseFlagError
+	}
+	if !checkExtPermmited(c.to, permmitedExts) {
+		fmt.Fprintln(c.ErrStream, "-to is only png, jpeg, jpg")
+		return ExitCodeParseFlagError
+	}
+	directory := flags.Args()[0]
+	if err := filepath.Walk(directory, c.delegateFileOperation); err != nil {
+		fmt.Fprintln(c.ErrStream, err)
+		return ExitCodeParseFlagError
+	}
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return ExitCodeParseFlagError
+	}
+	fmt.Fprintln(c.OutStream, "conver is succeed")
+	return ExitCodeOK
+}
+
+// 引数のディレクトリ以下を引数の関数で再帰的に処理する
+func (c *CLI) delegateFileOperation(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	if !info.IsDir() && filepath.Ext(path) == "."+c.from {
+		ConvertImage(path, AdaptExt(c.from), AdaptExt(c.to))
+	}
+	return nil
+}
+
+// 引数の拡張子が許可されているものならばtrue, それ以外なら false を返す
+func checkExtPermmited(ext string, permittedExts []string) bool {
+	for _, permmitedExt := range permittedExts {
+		if ext == permmitedExt {
+			return true
+		}
+	}
+	return false
+}
--- kadai2/tokunaga/cmd/imageConverter/main.go
+++ kadai2/tokunaga/cmd/imageConverter/main.go
@@ -1,65 +1,13 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/gopherdojo/dojo2/kadai2/tokunaga"
 )
 
-type Usage string
-
-var extTo string
-var extFrom string
-var usage Usage = "./imageConverter [-from=png|jpeg ] [-to=png|jpeg ] directory"
-var permmitedExts []string = []string{"png", "jpeg", "jpg"}
-
-// 引数の初期化
-func init() {
-	flag.StringVar(&extFrom, "from", "jpeg", string(usage))
-	flag.StringVar(&extTo, "to", "png", string(usage))
-}
-
 // メイン関数
 func main() {
-	flag.Parse()
-	if len(flag.Args()) != 1 {
-		fmt.Fprintln(os.Stderr, "Usage: "+string(usage))
-		os.Exit(1)
-	}
-	if !checkExtPermmited(extFrom, permmitedExts) {
-		fmt.Fprintln(os.Stderr, "-from is only png, jpeg, jpg")
-		os.Exit(1)
-	}
-	if !checkExtPermmited(extTo, permmitedExts) {
-		fmt.Fprintln(os.Stderr, "-to is only png, jpeg, jpg")
-		os.Exit(1)
-	}
-	directory := flag.Args()[0]
-	if err := filepath.Walk(directory, delegateFileOperation); err != nil {
-		os.Exit(1)
-	}
-}
-
-// 引数のディレクトリ以下を引数の関数で再帰的に処理する
-func delegateFileOperation(path string, info os.FileInfo, err error) error {
-	if err != nil {
-		return err
-	}
-	if !info.IsDir() && filepath.Ext(path) == "."+extFrom {
-		tokunaga.ConvertImage(path, tokunaga.AdaptExt(extFrom), tokunaga.AdaptExt(extTo))
-	}
-	return nil
-}
-
-// 引数の拡張子が許可されているものならばtrue, それ以外なら false を返す
-func checkExtPermmited(ext string, permittedExts []string) bool {
-	for _, permmitedExt := range permittedExts {
-		if ext == permmitedExt {
-			return true
-		}
-	}
-	return false
+	cli := &tokunaga.CLI{OutStream: os.Stdout, ErrStream: os.Stderr}
+	os.Exit(cli.Run(os.Args))
 }
