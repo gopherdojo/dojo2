9cc438e2a94b63e8342477035e91a1aec1a4b91d
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-06-28 02:58:19 +0900

    delegateFileOperationのテストケース追加

--- kadai2/tokunaga/cli_test.go
+++ kadai2/tokunaga/cli_test.go
@@ -1,6 +1,45 @@
 package tokunaga
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+)
+
+func TestCliDelegateFileOperation(t *testing.T) {
+	testdataDirPath := os.Getenv("GOPATH") + "/src/github.com/gopherdojo/dojo2/kadai2/test/testdata/"
+	testdirInfo, err := os.Stat(testdataDirPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	testfilePath := os.Getenv("GOPATH") + "/src/github.com/gopherdojo/dojo2/kadai2/test/testdata/image_shallow_A.png"
+	testfileInfo, err := os.Stat(testfilePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("erro != nil", func(t *testing.T) {
+		cli := CLI{}
+		input := "abort"
+		expected := "abort"
+		if actual := cli.delegateFileOperation("", nil, errors.New(input)); actual.Error() != expected {
+			t.Errorf("want delegateFileOperation(nil, nil, errors.New(%v)) = %v, got %v", input, expected, actual)
+		}
+	})
+	t.Run("target is dir", func(t *testing.T) {
+		cli := CLI{from: "jpeg"}
+		if actual := cli.delegateFileOperation(testdataDirPath, testdirInfo, nil); actual != nil {
+			t.Errorf("want delegateFileOperation(dirpath, dirfileinfo, nil) = nil, got %v", actual)
+		}
+	})
+	t.Run("target is file, file's ext is not target ext", func(t *testing.T) {
+		cli := CLI{from: "jpeg"}
+		if actual := cli.delegateFileOperation(testfilePath, testfileInfo, nil); actual != nil {
+			t.Errorf("want delegateFileOperation(filepath, fileinfo, nil) = nil, got %v", actual)
+		}
+	})
+
+}
 
 // 引数の拡張子が許可されているものならばtrue, それ以外なら false を返す
 func TestCheckExtPermmited(t *testing.T) {
