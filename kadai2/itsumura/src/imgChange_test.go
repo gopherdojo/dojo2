2426b2ffeeace8eb050b86d43c1168b8a53396a0
dumblepy <dumblepy@gmail.com>
2018-06-28 16:14:42 +0900

    テストに処理追加

--- kadai2/itsumura/src/imgChange_test.go
+++ kadai2/itsumura/src/imgChange_test.go
@@ -7,12 +7,21 @@ import (
 func TestExtension(t *testing.T) {
 	var f fileName = "aaa.txt"
 	ex := f.Extension()
-	println(ex)
+	if ex != "txt" {
+		t.Fatal(results)
+	}
 }
 
 func TestSearchDir(t *testing.T) {
 	dir := "../pic"
 	results, err := searchDir(dir)
+	if err != nil {
+		t.Fatal("failed test")
+		t.Fatal(results)
+	}
+
+	dir = "aaa"
+	results, err = searchDir(dir)
 	if err != nil {
 		t.Fatal("failed test")
 		t.Fatal(results)
