a6729f1d6342b335777b3d866afc19efd65c0990
mukai_masaki <mukai_masaki@cyberagent.co.jp>
2018-06-30 23:35:02 +0900

    range download.

--- kadai4/main.go
+++ kadai4/main.go
@@ -3,29 +3,69 @@ package main
 import (
 	"net/http"
 	"os"
-	"fmt"
 	"regexp"
+	"strconv"
+	"io/ioutil"
 )
 
 func main() {
-	req, err := http.NewRequest("GET", "https://kaboompics.com/cache/6/e/e/8/1/6ee81e1477ee1a9610149d0fe7fbd952213ba11d.jpeg?version=v3", nil)
+	url := "https://kaboompics.com/cache/6/e/e/8/1/6ee81e1477ee1a9610149d0fe7fbd952213ba11d.jpeg?version=v3"
+	fileSize, _, e := GetFileSize(url)
+	up := fileSize / 2
+	if e != nil {
+		os.Exit(1)
+	}
+	body1, _, err := RangeLoad(url, 0, up)
 	if err != nil {
 		os.Exit(1)
 	}
-	req.Header.Add("Range", "bytes=0-1")
+	body2, _, err := RangeLoad(url, up + 1, fileSize)
+	if err != nil {
+		os.Exit(1)
+	}
+	bytes := append(body1, body2...)
+	ioutil.WriteFile("image.jpeg", bytes, 0666)
+}
+
+func GetFileSize(url string) (max int, high int, err error) {
+	_, contentRange, err := RangeLoad(url, 0, 1)
+	return parseContentRange(contentRange)
+}
+
+func RangeLoad(url string, from int, to int) (body []byte, contentRange string, err error) {
+	errReturn := func(err error) (byte, string, error) {
+		return 0, "", err
+	}
+	var req *http.Request
+	req, err = http.NewRequest("GET", url, nil)
+	if err != nil {
+		errReturn(err)
+	}
+	req.Header.Add("Range", "bytes=" + strconv.Itoa(from) + "-" + strconv.Itoa(to))
 	client := new(http.Client)
 	resp, err := client.Do(req)
 	if err != nil {
-		os.Exit(1)
+		errReturn(err)
 	}
+	body, err = ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
- 	contentRange := resp.Header.Get("content-range")
-	compile := regexp.MustCompile("\\d-\\d/(\\d)")
 	if err != nil {
-		os.Exit(1)
+		errReturn(err)
+	}
+	contentRange = resp.Header.Get("content-range")
+	return body, contentRange, nil
+}
+
+func parseContentRange(contentRange string) (max int, high int, err error) {
+	compile := regexp.MustCompile("\\d+-(\\d+)/(\\d+)")
+	if err != nil {
+		return -1, -1, err
 	}
 	match := compile.FindSubmatch([]byte(contentRange))
-	bytes := match[1]
-	s := string(bytes)
-	fmt.Println(s)
+	high, err = strconv.Atoi(string(match[1]))
+	max, err = strconv.Atoi(string(match[2]))
+	if err != nil {
+		return -1, -1, err
+	}
+	return max, high, nil
 }
