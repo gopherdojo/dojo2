2fa8293025bf18c7641e7b62e8334849edff2a2c
mukai_masaki <mukai_masaki@cyberagent.co.jp>
2018-07-01 01:40:00 +0900

    refactoring.

--- kadai4/main.go
+++ kadai4/main.go
@@ -9,30 +9,51 @@ import (
 )
 
 func main() {
+	const Split = 10
 	url := "https://kaboompics.com/cache/6/e/e/8/1/6ee81e1477ee1a9610149d0fe7fbd952213ba11d.jpeg?version=v3"
 	fileSize, _, e := GetFileSize(url)
-	up := fileSize / 2
 	if e != nil {
 		os.Exit(1)
 	}
-	body1, _, err := RangeLoad(url, 0, up)
-	if err != nil {
-		os.Exit(1)
+	fileRanges := splitRange(fileSize, Split)
+	var bodies []byte
+	for _, r := range fileRanges {
+		body, _, err := RangeLoad(url, r)
+		if err != nil {
+			os.Exit(1)
+		}
+		bodies = append(bodies, body...)
 	}
-	body2, _, err := RangeLoad(url, up + 1, fileSize)
-	if err != nil {
-		os.Exit(1)
+	ioutil.WriteFile("image.jpeg", bodies, 0666)
+}
+
+// return "0-100"
+func splitRange(fileSize int, split int) []string {
+	aFileSize := fileSize / split
+	var ranges []string
+	start := 0
+	end := 0
+	for i := 0; i < split; i++ {
+		if i == 0 {
+			start = 0
+		} else {
+			start = end + 1
+		}
+		end = end + aFileSize
+		ranges = append(ranges,  strconv.Itoa(start) + "-" + strconv.Itoa(end))
 	}
-	bytes := append(body1, body2...)
-	ioutil.WriteFile("image.jpeg", bytes, 0666)
+	if fileSize % split != 0 {
+		ranges = append(ranges, strconv.Itoa(end + 1) + "-" + strconv.Itoa(fileSize))
+	}
+	return ranges
 }
 
 func GetFileSize(url string) (max int, high int, err error) {
-	_, contentRange, err := RangeLoad(url, 0, 1)
+	_, contentRange, err := RangeLoad(url, "0-1")
 	return parseContentRange(contentRange)
 }
 
-func RangeLoad(url string, from int, to int) (body []byte, contentRange string, err error) {
+func RangeLoad(url string, fileRange string) (body []byte, contentRange string, err error) {
 	errReturn := func(err error) (byte, string, error) {
 		return 0, "", err
 	}
@@ -41,7 +62,7 @@ func RangeLoad(url string, from int, to int) (body []byte, contentRange string, 
 	if err != nil {
 		errReturn(err)
 	}
-	req.Header.Add("Range", "bytes=" + strconv.Itoa(from) + "-" + strconv.Itoa(to))
+	req.Header.Add("Range", "bytes=" + fileRange)
 	client := new(http.Client)
 	resp, err := client.Do(req)
 	if err != nil {
