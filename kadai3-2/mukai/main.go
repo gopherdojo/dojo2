feb51bcba4f748aafa29edfdcd60eb305a8a4c46
mukai_masaki <mukai_masaki@cyberagent.co.jp>
2018-07-02 00:35:08 +0900

    contextでキャンセル.

--- kadai3-2/mukai/main.go
+++ kadai3-2/mukai/main.go
@@ -8,6 +8,9 @@ import (
 	"io/ioutil"
 	"fmt"
 	"time"
+	"os/signal"
+	"syscall"
+	"context"
 )
 
 type PartialData struct {
@@ -16,10 +19,23 @@ type PartialData struct {
 }
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(
+		signalCh,
+		syscall.SIGTERM,
+		syscall.SIGINT,
+	)
+	go func() {
+		select {
+		case <-signalCh:
+			cancel()
+		}
+	}()
 	start := time.Now().UnixNano()
 	const Split = 200
 	url := "https://www.noao.edu/image_gallery/images/d7/cygloop-8000.jpg"
-	fileSize, _, e := GetFileSize(url)
+	fileSize, _, e := GetFileSize(ctx, url)
 	if e != nil {
 		os.Exit(1)
 	}
@@ -28,9 +44,10 @@ func main() {
 	chs := make([]chan PartialData, 0)
 	for i, v := range fileRanges {
 		ch := make(chan PartialData)
-		go storePartialData(ch, url, i, v)
+		go storePartialData(ctx, ch, url, i, v)
 		chs = append(chs, ch)
 	}
+
 	for _, c := range chs {
 		data := <- c
 		bodies[data.index] = data.data
@@ -46,13 +63,13 @@ func main() {
 	fmt.Println((end - start) / 1000 / 1000)
 }
 
-func storePartialData(ch chan <- PartialData, url string, index int, fileRange string) {
-	body, _, err := RangeLoad(url, fileRange)
+func storePartialData(ctx context.Context, ch chan <- PartialData, url string, index int, fileRange string) {
+	body, _, err := RangeLoad(ctx, url, fileRange)
 	if err != nil {
 		os.Exit(1)
 	}
 	ch <- PartialData{index: index, data: body}
-	close(ch)
+	defer close(ch)
 }
 
 // return "0-100"
@@ -76,30 +93,31 @@ func splitRange(fileSize int, split int) []string {
 	return ranges
 }
 
-func GetFileSize(url string) (max int, high int, err error) {
-	_, contentRange, err := RangeLoad(url, "0-1")
+func GetFileSize(ctx context.Context, url string) (max int, high int, err error) {
+	_, contentRange, err := RangeLoad(ctx, url, "0-1")
 	return parseContentRange(contentRange)
 }
 
-func RangeLoad(url string, fileRange string) (body []byte, contentRange string, err error) {
-	errReturn := func(err error) (byte, string, error) {
-		return 0, "", err
+func RangeLoad(ctx context.Context, url string, fileRange string) (body []byte, contentRange string, err error) {
+	errReturn := func(err error) ([]byte, string, error) {
+		return nil, "", err
 	}
 	var req *http.Request
 	req, err = http.NewRequest("GET", url, nil)
 	if err != nil {
-		errReturn(err)
+		return errReturn(err)
 	}
+	req = req.WithContext(ctx)
 	req.Header.Add("Range", "bytes=" + fileRange)
 	client := new(http.Client)
 	resp, err := client.Do(req)
 	if err != nil {
-		errReturn(err)
+		return errReturn(err)
 	}
 	body, err = ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
 	if err != nil {
-		errReturn(err)
+		return errReturn(err)
 	}
 	contentRange = resp.Header.Get("content-range")
 	return body, contentRange, nil
