bec8bbd1a5c7a3ac6826cac916e84c7e235884c9
chillout2san <75463077+chillout2san@users.noreply.github.com>
2022-04-06 21:11:27 +0900

    refactor: チェックバック対応(validate関数を作成した)

--- kadai1/chillout2san/converter/converter.go
+++ kadai1/chillout2san/converter/converter.go
@@ -10,14 +10,14 @@ import (
 )
 
 /*
-	Convertは第三引数のディレクトリ以下を再起的に処理します。
+	第三引数のディレクトリ以下を再起的に処理します。
 	第一引数に与えられた拡張子と現在の拡張子が異なる場合、errorを返します。
 	第二引数に与えられた拡張子の画像に変更します。
 */
-func Convert(beforeExtension string, afterExtension string, targetDir string)error {
+func Convert(beforeExtension string, afterExtension string, targetDir string) error {
 	targetPathList := []string{}
 
-	err := filepath.WalkDir(targetDir, func(path string, info fs.DirEntry, err error)error {
+	err := filepath.WalkDir(targetDir, func(path string, info fs.DirEntry, err error) error {
 		if err != nil {
 			fmt.Println(err)
 			return nil
@@ -42,11 +42,11 @@ func Convert(beforeExtension string, afterExtension string, targetDir string)err
 		if presentExtension != "."+beforeExtension {
 			return errors.New("指定された拡張子が実際の拡張子と異なります。")
 		}
-	
+
 		dir, fileName := filepath.Split(targetPath)
-	
+
 		newFileName := strings.Replace(fileName, "."+beforeExtension, "."+afterExtension, 1)
-	
+
 		newPath := filepath.Join(dir, newFileName)
 		if err := os.Rename(targetPath, newPath); err != nil {
 			return err
--- kadai1/chillout2san/main.go
+++ kadai1/chillout2san/main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"dojo/kadai1/chillout2san/converter"
+	"errors"
 	"flag"
 	"log"
 )
+
+/*
+	ディレクトリが指定されていなかったり、拡張子がjpgもしくはpng以外で指定された際にerrorを返します。
+*/
+func validate(beforeExtension, afterExtension, targetDir *string) error {
+	if *beforeExtension != "jpg" && *beforeExtension != "png" {
+		return errors.New("変更前の拡張子はjpgもしくはpngのどちらかを指定してください。")
+	}
+
+	if *afterExtension != "jpg" && *afterExtension != "png" {
+		return errors.New("変更後の拡張子はjpgもしくはpngのどちらかを指定してください。")
+	}
+
+	if *targetDir == "" {
+		return errors.New("ディレクトリが指定されていません。")
+	}
+	return nil
+}
 
 func main() {
 	beforeExtension := flag.String("before", "jpg", "変換前の拡張子")
@@ -12,14 +31,11 @@ func main() {
 	targetDir := flag.String("path", "", "変換する写真のあるディレクトリ")
 	flag.Parse()
 
-	isPathEmpty := *targetDir == ""
-	if isPathEmpty {
-		log.Fatal("ディレクトリが指定されていません。")
+	if err := validate(beforeExtension, afterExtension, targetDir); err != nil {
+		log.Fatal(err)
 	}
 
-	err := converter.Convert(*beforeExtension, *afterExtension, *targetDir)
-
-	if err != nil {
+	if err := converter.Convert(*beforeExtension, *afterExtension, *targetDir); err != nil {
 		log.Fatal(err)
 	}
 }
