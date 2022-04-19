37bf95874cae47f72657f379b150601a0c3d3ae7
chillout2san <75463077+chillout2san@users.noreply.github.com>
2022-04-19 23:13:06 +0900

    kadai3: 完成

--- kadai3-1/chillout2san/main.go
+++ kadai3-1/chillout2san/main.go
@@ -8,11 +8,13 @@ import (
 	"time"
 )
 
-func main() {	
+func main() {
 	fmt.Println("タイピングゲーム！制限時間は30秒以内！")
 	timeLimit := time.After(time.Duration(30) * time.Second)
-	counter := int(0)
+	counter := 0
+	questionCounter := 0
 	outputChannel := make(chan string)
+	completeChannel := make(chan string)
 	wordList := []string{"guitar", "piano", "drum", "bass"}
 
 	// 問題を注入するgo-routine
@@ -25,18 +27,25 @@ func main() {
 	// 問題を出題して回答を受け付けるgo-routine
 	go func() {
 		scanner := bufio.NewScanner(os.Stdin)
-		output := <- outputChannel
-		fmt.Println(output)
-		
-		for scanner.Scan() {
+		for {
+			output := <-outputChannel
+			fmt.Println(output)
+			scanner.Scan()
+			questionCounter++
 			if output == scanner.Text() {
 				counter++
+			}
+			if questionCounter == len(wordList) {
+				completeChannel <- "done"
 			}
 		}
 	}()
 
 	select {
-	case <- timeLimit:
+	case <- completeChannel:
+		fmt.Println("全問題終了しました")
+		fmt.Println("正解数は" + strconv.Itoa(counter) + "個です")
+	case <-timeLimit:
 		fmt.Println("制限時間です。")
 		fmt.Println("正解数は" + strconv.Itoa(counter) + "個です")
 	}
